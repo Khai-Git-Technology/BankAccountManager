861b8dcb5efb2df0ccc360383cba85d10856e542
Julia Sylenok <silenokjulia1@gmail.com>
2025-04-29 13:13:54 +0300

    Реалізація методів інтерфейсу IBankAccount

---
+++ BankAccountManager.Core/BankAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountManager.Core
+{
+    public class BankAccount : IBankAccount
+    {
+        private decimal balance;
+        private decimal loanBalance;
+        private List<string> transactionHistory = new();
+        private bool isAuthenticated = false;
+        private readonly INotification notificationService;
+
+        public void Deposit(decimal amount)
+        {
+            balance += amount;
+            AddTransaction($"Поповнення: {amount} грн");
+            notificationService.SendBalanceChangeNotification(balance);
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount > balance)
+            {
+                Console.WriteLine("Недостатньо коштів!");
+                return;
+            }
+            balance -= amount;
+            AddTransaction($"Зняття: {amount} грн");
+            notificationService.SendBalanceChangeNotification(balance);
+        }
+
+        public decimal GetBalance() => balance;
+
+
+        public void AddTransaction(string details)
+        {
+            transactionHistory.Add(details);
+            notificationService.SendTransactionNotification(details);
+        }
+
+    }
+}
